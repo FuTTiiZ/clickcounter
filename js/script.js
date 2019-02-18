f642716bec23f10f5b872759c3e0d4e614e91449
FuTTiiZ <46918659+FuTTiiZ@users.noreply.github.com>
2019-02-18 17:14:52 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-var clicks=0;function add(){clicks++,document.getElementById("btn-main").value="Clicks: "+clicks}
+var clicks=0;function add(){clicks++,document.getElementById("body-main").innerHTML="Clicks: "+clicks}
